9a5f7e0116d31eb0f61e44fb60c7439e798184ff
Shubham Dhage <shubhamdhage930@gmail.com>
2019-02-24 21:53:39 +0530

    Updates

---
+++ precache-manifest.14048a42faaa3ce598eb6f460a6264e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3a71aa2d694588606c672ad1ea0a279",
+    "url": "/article-blog/static/media/10.a3a71aa2.jpg"
+  },
+  {
+    "revision": "30c5e5b573dd18bc9ecf",
+    "url": "/article-blog/static/js/runtime~main.30c5e5b5.js"
+  },
+  {
+    "revision": "619e3d7a5d6d6ea45b19",
+    "url": "/article-blog/static/js/main.619e3d7a.chunk.js"
+  },
+  {
+    "revision": "8bb6cf48ce5cd9607dbf",
+    "url": "/article-blog/static/js/2.8bb6cf48.chunk.js"
+  },
+  {
+    "revision": "619e3d7a5d6d6ea45b19",
+    "url": "/article-blog/static/css/main.30813aeb.chunk.css"
+  },
+  {
+    "revision": "8bb6cf48ce5cd9607dbf",
+    "url": "/article-blog/static/css/2.7df53188.chunk.css"
+  },
+  {
+    "revision": "e80c5220ea54d087944dbc96a0f90a37",
+    "url": "/article-blog/index.html"
+  }
+];
